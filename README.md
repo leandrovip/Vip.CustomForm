a7cb6d553aaa46935b11a6cbc9917da275d5da1d
Leandro Ferreira <leandro@vipsolucoes.com>
2020-04-30 03:55:27 -0300

    Create README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vip.CustomForm
+
+A simple form customizable for Windows Forms. :computer:
+
+![Example](form-demo.png "Example form")
+
+## License
+
+MIT
+
--- src/Vip.CustomForm.Demo/frmDemo.Designer.cs
+++ src/Vip.CustomForm.Demo/frmDemo.Designer.cs
@@ -37,7 +37,7 @@
             this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
             this.CaptionFont = new System.Drawing.Font("Century Gothic", 10F);
             this.CaptionForeColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1068, 554);
+            this.ClientSize = new System.Drawing.Size(488, 464);
             this.ForeColor = System.Drawing.Color.Black;
             this.InnerBorderVisibility = false;
             this.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
